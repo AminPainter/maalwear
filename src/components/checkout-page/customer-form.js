397c534d7e874de92a3bbf0e4921685acd225fba
AminPainter <aminpainter786@gmail.com>
2023-05-25 15:15:32 +0530

    saving form inputs to react context

--- src/components/checkout-page/customer-form.js
+++ src/components/checkout-page/customer-form.js
@@ -1,41 +1,68 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { Button, Stack, TextField } from '@mui/material';
-import { Heading, Icon } from 'ui';
 
-const CustomerForm = () => {
+import { Grid, Heading, Icon } from 'ui';
+
+const CustomerForm = ({ checkoutDetails, setCheckoutDetails, setCurrentStep }) => {
   const initialValues = {
-    fullName: '',
-    email: '',
+    firstName: checkoutDetails.firstName,
+    lastName: checkoutDetails.lastName,
+    email: checkoutDetails.email,
+  };
+
+  const handleSubmit = values => {
+    setCheckoutDetails(prevDetails => ({
+      ...prevDetails,
+      firstName: values.firstName,
+      lastName: values.lastName,
+      email: values.email,
+    }));
+
+    setCurrentStep(prevStep => prevStep + 1);
   };
 
   return (
-    <Formik initialValues={initialValues}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       {formik => (
         <Stack gap={2} width='50%' component='form' onSubmit={formik.handleSubmit}>
           <Heading textAlign='center' variant='secondary'>
             customer information
           </Heading>
-          <TextField
-            name='fullName'
-            label='Full Name'
-            variant='outlined'
-            value={formik.values.fullName}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            required
-          />
 
-          <TextField
-            type='email'
-            name='email'
-            label='Email Address'
-            variant='outlined'
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            required
-          />
+          <Grid columns={2} gap={2}>
+            <TextField
+              name='firstName'
+              label='First Name'
+              variant='outlined'
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
+            />
+
+            <TextField
+              name='lastName'
+              label='Last Name'
+              variant='outlined'
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
+            />
+
+            <TextField
+              type='email'
+              name='email'
+              label='Email Address'
+              variant='outlined'
+              sx={{ gridColumn: '1 / -1' }}
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
+            />
+          </Grid>
 
           <Button
             type='submit'
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Stack, Step, StepLabel, Stepper, styled } from '@mui/material';
+import { Box, Button, Container, Stack, Step, StepLabel, Stepper, styled } from '@mui/material';
 
 import commerce from 'config/commerce';
-import { useCart } from 'hooks';
-import { AddressForm, CustomerForm } from 'components/checkout-page';
 import SiteSeo from 'components/seo';
+import { useCart } from 'hooks';
+import { Icon } from 'ui';
+import { AddressForm, CustomerForm } from 'components/checkout-page';
 
 const steps = ['Customer Information', 'Shipping Address', 'Payment'];
 
 const CheckoutPage = () => {
   const { cart } = useCart();
   const [currentStep, setCurrentStep] = useState(0);
-  const [token, setToken] = useState();
+  const [checkoutDetails, setCheckoutDetails] = useState({});
 
-  useEffect(() => {
-    (async () => {
-      if (!cart?.line_items.length) return;
+  // useEffect(() => {
+  //   (async () => {
+  //     if (!cart?.line_items.length) return;
 
-      const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
-      setToken(token);
-    })();
-  }, [cart]);
+  //     const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+  //     setToken(token);
+  //   })();
+  // }, [cart]);
 
   return (
     <Stack height='85vh' justifyContent='center'>
       <CheckoutContainer>
+        <Box minHeight='5.5vh'>
+          {currentStep !== 0 && (
+            <Button
+              onClick={() => setCurrentStep(currentStep - 1)}
+              startIcon={<Icon name='ArrowLeft' />}>
+              back
+            </Button>
+          )}
+        </Box>
+
         <Stepper activeStep={currentStep} alternativeLabel>
           {steps.map(label => (
             <Step key={label}>
@@ -37,9 +48,15 @@ const CheckoutPage = () => {
           {(() => {
             switch (currentStep) {
               case 0:
-                return <CustomerForm />;
+                return (
+                  <CustomerForm
+                    checkoutDetails={checkoutDetails}
+                    setCheckoutDetails={setCheckoutDetails}
+                    setCurrentStep={setCurrentStep}
+                  />
+                );
               case 1:
-                return <AddressForm />;
+                return <AddressForm setCheckoutDetails={setCheckoutDetails} />;
               default:
                 return <></>;
             }
@@ -59,5 +76,5 @@ const CheckoutContainer = styled(Container)(({ theme }) => ({
   minHeight: '60vh',
   boxShadow: theme.shadows[12],
   borderRadius: theme.borderRadius.tiny,
-  padding: theme.spacing(3, 5),
+  padding: theme.spacing(3, 5, 10),
 }));
